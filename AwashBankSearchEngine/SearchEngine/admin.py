d8783da5b44e85ce0f604d22e5ae87bedd5d7fde
Gadisa Teka <92205109+hinsoo12@users.noreply.github.com>
2022-10-10 14:54:47 +0300

    Update admin.py changing registered models

--- AwashBankSearchEngine/SearchEngine/admin.py
+++ AwashBankSearchEngine/SearchEngine/admin.py
@@ -3,8 +3,7 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(discussionBoard)
+# Register your models here.
+
 admin.site.register(Category)
-admin.site.register(Announcements)
-admin.site.register(systemAdministrators)
 admin.site.register(FileAndDocumentBase)
