6fe8e3b7f862834d6f53551138ddf65c68a9320c
hinsoo12 <gado8teka@gmail.com>
2022-10-10 16:12:54 +0300

    Committed

--- AwashBankSearchEngine/SearchEngine/views.py
+++ AwashBankSearchEngine/SearchEngine/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def loginInterface(request):
-    return HttpResponse("this is the login page for the Admin")
+    return HttpResponse("this is the login page for the Employeers")
 
 def fileUploadPage(request):
     return HttpResponse("where the admin uploads a file")
