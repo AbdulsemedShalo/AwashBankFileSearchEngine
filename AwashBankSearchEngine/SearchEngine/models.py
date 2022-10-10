e3ee682501bb4fcd73a85882e902412022926f2b
Gadisa Teka <92205109+hinsoo12@users.noreply.github.com>
2022-10-10 14:56:30 +0300

    Update models.py

--- AwashBankSearchEngine/SearchEngine/models.py
+++ AwashBankSearchEngine/SearchEngine/models.py
@@ -12,32 +12,6 @@ class FileAndDocumentBase(models.Model):
     def _str_(self):
         return self.fileContent
 
-# a model class to create system admins profile base to perform CRUD operations
-class systemAdministrators(models.Model):
-    firstName=models.CharField(max_length=100)
-    LastName=models.CharField(max_length=100)
-    Role=models.CharField(max_length=100)
-    jobPosition=models.CharField(max_length=100)
-    profilePicture=models.FileField()
-    userName=models.CharField(max_length=100)
-    PassWord=models.CharField(max_length=100)
-    publicaitonDate=models.DateTimeField("PublicationDate")
-    publishedBy=models.CharField(max_length=100)
-
-    def _str_(self):
-        return self.firstName
-
-#a notificaiton post by admin for the system users
-class Announcements(models.Model):
-    Title=models.CharField(max_length=100)
-    MessageContent=models.TextField()
-    publicaitonDate=models.DateTimeField("PublicationDate")
-    publishedBy=models.CharField(max_length=100)
-
-    def _str_(self):
-        return self.MessageContent
-
-
 # a model for creating the categories under which files and documents are indexed
 class Category(models.Model):
     categoryTitle=models.CharField(max_length=100)
@@ -48,13 +22,6 @@ class Category(models.Model):
     
 
 
-#a model creation for the discussion panel where users can raise or create a post that is shared and answered by the admin
-class discussionBoard(models.Model):
-    boardMessage=models.TextField()
-    publicaitonDate=models.DateTimeField("PublicationDate")
-    publishedBy=models.CharField(max_length=100)
 
-    def _str_(self):
-        return self.boardMessage
 
 
